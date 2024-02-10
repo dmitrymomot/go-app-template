8899113642d3d8964136d9ba5c22cd55fe9a4ecc
Dmitry Momot <mail@dmomot.com>
2024-02-10 22:22:36 +0200

    Add Tailwind CSS and configuration file

---
+++ web/tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './web/views/**/*.{html,js,ts,templ,go}',
+    './web/components/**/*.{html,js,ts,templ,go}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
+

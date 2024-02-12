e0bc98244d6c7aeebe0a13e0797a9ec65deb6a44
Dmitry Momot <mail@dmomot.com>
2024-02-12 23:53:54 +0200

    Register types for gob serialization in init function

--- cmd/app/init.go
+++ cmd/app/init.go
@@ -5,8 +5,11 @@ import (
 	"net/url"
 )
 
+// init is a special function in Go that is automatically called before the main function.
+// It is commonly used for initialization tasks such as registering types or setting up global variables.
+// In this case, the init function is registering various types for gob serialization.
 func init() {
-	// Register the types for gob
+	// Register the types for gob serialization
 	gob.Register(url.Values{})
 	gob.Register(map[string]string{})
 	gob.Register(map[string][]string{})
